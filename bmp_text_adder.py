c66ae5bee639aac6b8cae36d577e0de626a6b736
or-mv <or.gamliel@mantis-vision.com>
2023-08-23 17:18:33 +0300

    added dynamism in fitting correct locations of text regarding the inputted picture

--- bmp_text_adder.py
+++ bmp_text_adder.py
@@ -6,10 +6,16 @@ def add_text_to_image(input_image_path, output_image_path, above_text, below_tex
     # Open an Image
     img = Image.open(input_image_path)
 
-    # Calculate the new width and height to accommodate the additional text
+    # Calculate the text dimensions and the new image dimensions
     original_width, original_height = img.size
+    font_above = ImageFont.truetype("arial.ttf", 16, encoding="unic")
+    font_below = ImageFont.truetype("arial.ttf", below_text_size, encoding="unic")
+
+    above_text_width, above_text_height = font_above.getsize(above_text)
+    below_text_width, below_text_height = font_below.getsize(below_text)
+
     new_width = original_width + 80  # Increase width by 80 pixels
-    new_height = original_height + 80  # Increase height by 80 pixels
+    new_height = original_height + 80 + above_text_height + below_text_height  # Include space for text
 
     # Create a new image with the calculated width and height
     new_img = Image.new("RGB", (new_width, new_height), color=(255, 255, 255))
@@ -25,12 +31,10 @@ def add_text_to_image(input_image_path, output_image_path, above_text, below_tex
     font_below = ImageFont.truetype("arial.ttf", below_text_size, encoding="unic")
 
     # Add above text to the image
-    text_width, text_height = draw.textsize(above_text, font=font_above)
-    draw.text(((new_width - text_width) // 2, 7), above_text, font=font_above, fill='black')
+    draw.text(((new_width - above_text_width) // 2, 10), above_text, font=font_above, fill='black')
 
     # Add below text to the image
-    text_width, text_height = draw.textsize(below_text, font=font_below)
-    draw.text(((new_width - text_width) // 2, new_height - text_height - 10), below_text, font=font_below, fill='black')
+    draw.text(((new_width - below_text_width) // 2, new_height - below_text_height - 10), below_text, font=font_below, fill='black')
 
     # Save the edited image
     new_img.save(output_image_path)
@@ -49,8 +53,8 @@ output_image_path = r"C:\Users\user\Desktop\bmp\dfg_text.png"
 above_text = "SCAN TO DISPLAY HOLOGRAM"
 below_text = "Having issues? Visit Summitov.com/portal."
 
-# Add text above and below the image with smaller below text and get the path of the saved image
-saved_image_path = add_text_to_image(input_image_path, output_image_path, above_text, below_text, below_text_size=10)
+# Add text above and below the image and get the path of the saved image
+saved_image_path = add_text_to_image(input_image_path, output_image_path, above_text, below_text)
 
 # Print the path of the saved image for the user
 print("Image saved at:", saved_image_path)

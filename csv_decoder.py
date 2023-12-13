695d7b39234d1dc1e9364dc8fc5043c7899d3e30
or-mv <or.gamliel@mantis-vision.com>
2023-12-13 15:29:15 +0200

    added plot for epi, need to add rate

--- csv_decoder.py
+++ csv_decoder.py
@@ -1,91 +1,136 @@
 import pandas as pd
+import plotly.express as px
 import plotly.graph_objs as go
 from plotly.subplots import make_subplots
+import matplotlib.pyplot as plt
+
 
 
 def read_and_process_csv(csv_path):
-    # Adjust the CSV path if it contains double backslashes
     csv_path = csv_path.replace("\\\\", "\\")
-
-    # Remove double quotes if present
     csv_path = csv_path.strip('"')
-
     df = pd.read_csv(csv_path)
-
     unique_stream_ids = df['StreamId'].unique()
+    return df, unique_stream_ids
 
-    def plot_scale_figure(stream_id):
-        stream_data_scale = df[(df['StreamId'] == stream_id) & (df['ScaleInMm'] != 0)]
+def plot_scale_figure(df, stream_id):
+    stream_data_scale = df[(df['StreamId'] == stream_id) & (df['ScaleInMm'] != 0)]
+    fig_scale = go.Figure()
 
-        fig_scale = go.Figure()
+    if not stream_data_scale.empty:
+        average_scale = stream_data_scale['ScaleInMm'].mean()
+        fig_scale.add_trace(go.Scatter(x=stream_data_scale.index, y=stream_data_scale['ScaleInMm'],
+                                      mode='markers', marker=dict(size=10), name='ScaleInMm'))
+        fig_scale.add_annotation(text=f'Average ScaleInMm: {average_scale:.2f}', x=0.5, y=0.9,
+                                 xref='paper', yref='paper', showarrow=False, font=dict(size=14))
 
-        if not stream_data_scale.empty:
-            average_scale = stream_data_scale['ScaleInMm'].mean()
-            fig_scale.add_trace(go.Scatter(x=stream_data_scale.index, y=stream_data_scale['ScaleInMm'],
-                                          mode='markers', marker=dict(size=10), name='ScaleInMm'))
-            fig_scale.add_annotation(text=f'Average ScaleInMm: {average_scale:.2f}', x=0.5, y=0.9,
-                                     xref='paper', yref='paper', showarrow=False, font=dict(size=14))
+    fig_scale.update_layout(title=f'Stream {stream_id} - ScaleInMm',
+                            xaxis_title='Frames',
+                            yaxis_title='Millimeters',
+                            showlegend=True)
 
-        fig_scale.update_layout(title=f'Stream {stream_id} - ScaleInMm',
-                                xaxis_title='Frames',
-                                yaxis_title='Millimeters',
-                                showlegend=True)
+    return fig_scale
 
-        return fig_scale
+def plot_error_figures(df, stream_id):
+    stream_data = df[(df['StreamId'] == stream_id)]
+    fig_errors = go.Figure()
 
-    def plot_error_figures(stream_id):
-        stream_data = df[(df['StreamId'] == stream_id)]
+    additional_columns = ['Col1AvgError', 'Col2AvgError', 'C2CAvgError']
 
-        fig_errors = go.Figure()
+    for col in additional_columns:
+        stream_data_col = stream_data[(stream_data[col] != 0)]
+        if not stream_data_col.empty:
+            average_col = stream_data_col[col].mean()
+            fig_errors.add_trace(go.Scatter(x=stream_data_col.index, y=stream_data_col[col],
+                                           mode='markers', marker=dict(size=10), name=col))
+            fig_errors.add_annotation(text=f'Average {col}: {average_col:.2f}', x=0.5,
+                                      y=0.9 - additional_columns.index(col) * 0.1,
+                                      xref='paper', yref='paper', showarrow=False, font=dict(size=14))
 
-        # Additional columns to include
-        additional_columns = ['Col1AvgError', 'Col2AvgError', 'C2CAvgError']
+    calibration_rate = calculate_calibration_rate(stream_data['ScaleInMm'].mean(),
+                                                  stream_data['Col1AvgError'].mean(),
+                                                  stream_data['Col2AvgError'].mean(),
+                                                  stream_data['C2CAvgError'].mean())
 
-        for col in additional_columns:
-            stream_data_col = stream_data[(stream_data[col] != 0)]
-            if not stream_data_col.empty:
-                average_col = stream_data_col[col].mean()
-                fig_errors.add_trace(go.Scatter(x=stream_data_col.index, y=stream_data_col[col],
-                                               mode='markers', marker=dict(size=10), name=col))
-                fig_errors.add_annotation(text=f'Average {col}: {average_col:.2f}', x=0.5,
-                                          y=0.9 - additional_columns.index(col) * 0.1,
-                                          xref='paper', yref='paper', showarrow=False, font=dict(size=14))
+    fig_errors.add_annotation(text=f'<b style="color: green;"> Calibration Rate: {calibration_rate:.2f}</b>',
+                              x=0.5, y=1, xref='paper', yref='paper', showarrow=False, font=dict(size=16))
 
-        calibration_rate = calculate_calibration_rate(stream_data['ScaleInMm'].mean(),
-                                                      stream_data['Col1AvgError'].mean(),
-                                                      stream_data['Col2AvgError'].mean(),
-                                                      stream_data['C2CAvgError'].mean())
+    fig_errors.update_layout(title=f'Stream {stream_id} - Error Values',
+                             xaxis_title='Frames',
+                             yaxis_title='Pixels',
+                             showlegend=True)
 
-        fig_errors.add_annotation(text=f'<b style="color: green;"> Calibration Rate: {calibration_rate:.2f}</b>',
-                                  x=0.5, y=1, xref='paper', yref='paper', showarrow=False, font=dict(size=16))
+    return fig_errors
 
-        fig_errors.update_layout(title=f'Stream {stream_id} - Error Values',
-                                 xaxis_title='Frames',
-                                 yaxis_title='Pixels',
-                                 showlegend=True)
+def calculate_calibration_rate(average_scale, avg_error_col1, avg_error_col2, avg_error_c2c):
+    rate = 100
 
-        return fig_errors
+    if abs(average_scale - 322) >= 2:
+        rate -= 20
 
-    def calculate_calibration_rate(average_scale, avg_error_col1, avg_error_col2, avg_error_c2c):
-        rate = 100
+    for avg_error in [avg_error_col1, avg_error_col2, avg_error_c2c]:
+        if avg_error > 2:
+            rate -= 5 * ((avg_error - 2) // 0.5)
 
-        if abs(average_scale - 322) >= 2:
-            rate -= 20
-
-        for avg_error in [avg_error_col1, avg_error_col2, avg_error_c2c]:
-            if avg_error > 2:
-                rate -= 5 * ((avg_error - 2) // 0.5)
-
-        return max(0, rate)
-
-    # Iterate over unique_stream_ids after defining it
-    for stream_id in unique_stream_ids:
-        fig_scale = plot_scale_figure(stream_id)
-        fig_errors = plot_error_figures(stream_id)
-
-        fig_scale.show()
-        fig_errors.show()
+    return max(0, rate)
 
 # Get the CSV file path from the user
 csv_path = input("Please provide the path to the CSV file: ")
-read_and_process_csv(csv_path)
+
+# Call the function and get df, unique_stream_ids
+df, unique_stream_ids = read_and_process_csv(csv_path)
+
+# Iterate over unique_stream_ids after defining it
+for stream_id in unique_stream_ids:
+    fig_scale = plot_scale_figure(df, stream_id)
+    fig_errors = plot_error_figures(df, stream_id)
+
+    fig_scale.show()
+    fig_errors.show()
+
+    # # Check if the required columns exist in the DataFrame
+    # if 'FrameID' not in df.columns:
+    #     print("Error: 'FrameID' column is missing in the DataFrame.")
+    # if 'MedianEpiDist' not in df.columns:
+    #     print("Error: 'MedianEpiDist' column is missing in the DataFrame.")
+    # if 'spreadEpiDist' not in df.columns:
+    #     print("Error: 'spreadEpiDist' column is missing in the DataFrame.")
+
+    # Create a new figure for FrameID, medianEpiDist, spreadEpiDist
+    fig_epi = go.Figure()
+
+    # Check if the new columns exist in the DataFrame
+    if 'FrameID' in df.columns and 'medianEpiDist' in df.columns and 'spreadEpiDist' in df.columns:
+        # Plot medianEpiDist
+        fig_epi.add_trace(go.Scatter(x=df['FrameID'], y=df['medianEpiDist'], mode='lines+markers',
+                                    marker=dict(color='pink', size=8), name='MedianEpiDist'))
+
+        # Plot spreadEpiDist
+        fig_epi.add_trace(go.Scatter(x=df['FrameID'], y=df['spreadEpiDist'], mode='lines',
+                                    line=dict(color='turquoise', dash='dash'), name='SpreadEpiDist'))
+
+        # Annotate average value for medianEpiDist
+        average_median = df['medianEpiDist'].mean()
+        fig_epi.add_annotation(text=f'Average MedianEpiDist: {average_median:.2f}', x=0.5, y=0.9,
+                                xref='paper', yref='paper', showarrow=False, font=dict(size=14),
+                                bgcolor='rgba(255, 255, 255, 0.5)')
+
+        # Annotate average value for spreadEpiDist
+        average_spread = df['spreadEpiDist'].mean()
+        fig_epi.add_annotation(text=f'Average SpreadEpiDist: {average_spread:.2f}', x=0.5, y=0.8,
+                                xref='paper', yref='paper', showarrow=False, font=dict(size=14),
+                                bgcolor='rgba(255, 255, 255, 0.5)')
+
+        fig_epi.update_layout(title=f'Stream {stream_id} - Epi Values',
+                            xaxis_title='Frame',
+                            yaxis_title='Millimeters',
+                            showlegend=True)
+
+        fig_epi.show()
+    else:
+        print("One or more columns (FrameID, MedianEpiDist, SpreadEpiDist) are missing in the DataFrame.")
+
+
+
+
+

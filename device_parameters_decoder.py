0e2c53052f8aa39719adaa644a9114d2c7da79dd
or-mv <or.gamliel@mantis-vision.com>
2024-01-07 16:37:34 +0200

    added new features for both scripts, now the exe will also call the plotting

--- device_parameters_decoder.py
+++ device_parameters_decoder.py
@@ -1,6 +1,41 @@
+import sys
+import os
 import matplotlib.pyplot as plt
 import re
 import mplcursors
+
+def is_valid_path(path):
+    """
+    Check if the given path is a valid file path.
+    """
+    return os.path.isfile(path)
+
+# Check if log file path is provided as a command-line argument
+if len(sys.argv) == 2:
+    log_file = sys.argv[1]
+    if not is_valid_path(log_file):
+        print("Invalid path. Please enter a valid file path.")
+        sys.exit(1)
+else:
+    # Ask the user for the log file path if not provided or invalid
+    try:
+        log_file = input("Enter the path to your log file: ")
+        while not is_valid_path(log_file):
+            print("Invalid path. Please enter a valid file path.")
+            log_file = input("Enter the path to your log file: ")
+    except EOFError:
+        sys.exit(1)
+
+# Read the log file and parse the data using regular expressions
+data = {}
+with open(log_file, 'r') as file:
+    lines = file.readlines()
+    for line in lines:
+        match = re.match(r'\s*([\d.-]+)\s*:\s*(.*)', line)
+        if match:
+            value = float(match.group(1))
+            key = match.group(2).strip()
+            data[key] = value
 
 # Define the parameter ranges
 ranges = {
@@ -34,20 +69,6 @@ ranges = {
     'IR Camera FOV-y [°]': (60, 70),
 }
 
-# Get the log file path from the user
-log_file = input("Enter the path to your log file: ")
-
-# Read the log file and parse the data using regular expressions
-data = {}
-with open(log_file, 'r') as file:
-    lines = file.readlines()
-    for line in lines:
-        match = re.match(r'\s*([\d.-]+)\s*:\s*(.*)', line)
-        if match:
-            value = float(match.group(1))
-            key = match.group(2).strip()
-            data[key] = value
-
 # Plotting and evaluation
 fig, ax = plt.subplots(figsize=(10, 12))
 bar_colors = ['green' if ranges[param][0] <= value <= ranges[param][1] else 'red' for param, value in data.items()]
@@ -79,6 +100,9 @@ ax.set_xlabel('Values')
 # Set the main title
 title = f'Successes: {successes} (Within Range), Failures: {failures} (Out of Range)'
 ax.set_title(title, pad=30, color='black')  # Adjust title color here
+
+# Display the input at the top right corner
+plt.annotate(f'Input: {log_file}', xy=(1, 1), xytext=(-10, -10), ha='right', va='top', color='black', xycoords='axes fraction', textcoords='offset points')
 
 # Display the plot
 plt.tight_layout(rect=[0, 0.03, 1, 0.95])
